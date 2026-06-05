1f015224fd016afa2b77a9bdadcbbc7eaad1e912
souradeep <souradeepm.11@gmail.com>
2026-06-05 10:19:36 +0530

    sumOfCubes added lab1ex1

--- src/com/souradeep/lab1/ex1/SumofCubes.java
+++ src/com/souradeep/lab1/ex1/SumofCubes.java
@@ -1,10 +1,25 @@
 package com.souradeep.lab1.ex1;
 
+import java.util.Scanner;
+
 public class SumofCubes {
+	
+	public static int cubesum(int n) {
+		int s = 0;
+		int d = 0;
+		while(n!=0) {
+			d = n%10;
+			s = s+(d*d*d);
+			n = n/10;
+		}
+		return s;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number: ");
+		int n = sc.nextInt();
+		System.out.println("The sum of the cubes is = "+SumofCubes.cubesum(n));
 	}
 
 }
